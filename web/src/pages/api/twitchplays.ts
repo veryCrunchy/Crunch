c138ba2d14bb8217dfc77bb0e5e015df9a79cd9b
veryCrunchy <verycrunchydev@gmail.com>
2023-06-23 01:12:10 +0200

    Twitchplays api JSON validatior

--- web/src/pages/api/twitchplays.ts
+++ web/src/pages/api/twitchplays.ts
@@ -1,8 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { cache } from "~/utils/api";
-
+interface Data {
+  name: string;
+  inputs: string[];
+}
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
+    const data: Data[] = req.body as Data[];
+
+    if (!isValidData(data)) {
+      res.status(400).json({ message: "Invalid JSON data" });
+      return;
+    }
     cache.set("twitchplays", req.body, 600);
     res
       .status(200)
@@ -10,4 +19,27 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   } else {
     res.status(405).json({ message: "Method not allowed" });
   }
+}
+
+function isValidData(data: any): data is Data[] {
+  if (!Array.isArray(data)) {
+    return false;
+  }
+
+  for (const item of data) {
+    if (
+      typeof item !== "object" ||
+      item === null ||
+      !("name" in item) ||
+      !("inputs" in item)
+    ) {
+      return false;
+    }
+
+    if (typeof item.name !== "string" || !Array.isArray(item.inputs)) {
+      return false;
+    }
+  }
+
+  return true;
 }
--- web/src/pages/greasymac/twitchplays/index.tsx
+++ web/src/pages/greasymac/twitchplays/index.tsx
@@ -23,7 +23,19 @@ const Home: NextPage = () => {
         />
         <meta name="theme-color" content="#794ec4" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="icon" href="/favicon.greasymac.ico" />
       </Head>
       <style jsx global>{`
         html {
