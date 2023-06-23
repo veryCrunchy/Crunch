70183832090dba541db6ac052bb76a33fd77512b
veryCrunchy <verycrunchydev@gmail.com>
2023-06-23 04:07:47 +0200

    Overlay

--- web/src/pages/api/twitchplays.ts
+++ web/src/pages/api/twitchplays.ts
@@ -11,10 +11,12 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (!isValidData(data)) {
       res.status(400).json({ message: "Invalid JSON data" });
       return;
-    } else cache.set("twitchplays", req.body, 600);
-    res
-      .status(200)
-      .json({ message: "TwitchPlays inputs successfully received" });
+    } else {
+      cache.set("twitchplays", req.body, 80);
+      res
+        .status(200)
+        .json({ message: "TwitchPlays inputs successfully received" });
+    }
   } else {
     res.status(405).json({ message: "Method not allowed" });
   }
--- web/src/pages/greasymac/twitchplays/overlay/index.tsx
+++ web/src/pages/greasymac/twitchplays/overlay/index.tsx
@@ -2,7 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
 import { Inter } from "next/font/google";
-import { useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,19 +12,19 @@ const Home: NextPage = () => {
     inputs: string[];
   }
 
-  const { data, refetch } = api.twitchplays.get.useQuery<Data[]>();
-
+  const { data, refetch, remove } = api.twitchplays.get.useQuery<Data[]>();
+  const inputsRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      refetch().catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    setInterval(() => {
+      refetch()
+        .then((res) => {
+          if (res.status == "error") remove();
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
     }, 10000); // Fetch every 10 seconds (10000 milliseconds)
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [refetch]);
+  }, [refetch, remove]);
 
   if (!data)
     return (
@@ -67,7 +67,7 @@ const Home: NextPage = () => {
           border-radius: 16px;
           box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
           -webkit-backdrop-filter: blur(17.1px);
-          border: 1px solid rgba(122, 61, 212, 0.33);
+          border: 2px solid rgba(193, 157, 251, 0.6);
         }
 
         html {
@@ -87,8 +87,8 @@ const Home: NextPage = () => {
         }
 
         #inputs code {
-          background: rgba(122, 70, 199, 0.08);
-          border: 1px solid rgba(193, 157, 251, 0.3);
+          background: rgba(122, 70, 199, 0.2);
+          border: 2px solid rgba(193, 157, 251, 0.4);
           border-radius: 4px;
           color: #c19dfb;
           font-size: 1.1rem;
@@ -99,27 +99,29 @@ const Home: NextPage = () => {
         }
       `}</style>
       <main className="flex min-h-screen justify-center">
-        <div className="container flex max-w-fit flex-col items-center justify-center">
-          <div className="twitchplays_card_overlay h-auto w-screen rounded-xl bg-[#0d1117]/90 py-2">
-            <div className="mx-auto flex flex-col " id="inputs">
-              {(data as Data[])?.map((input: Data) => {
+        <div className="container flex max-w-fit flex-col">
+          <div
+            className={
+              "twitchplays_card_overlay h-auto w-screen rounded-xl bg-[#0d1117]/90 py-2"
+            }
+          >
+            <div ref={inputsRef} className="mx-auto flex flex-col " id="inputs">
+              {(data as Data[])?.map((input: Data, index) => {
                 return (
-                  <>
-                    <ul
-                      key={input.name}
-                      className="my-[0.12rem] flex flex-row flex-wrap gap-1 whitespace-nowrap uppercase leading-none"
-                    >
-                      <strong>{input.name}:</strong>
-                      <br className="sm:hidden" />{" "}
-                      {input.inputs.map((input) => {
-                        return (
-                          <>
-                            <code className="px-1 py-1">{input}</code>
-                          </>
-                        );
-                      })}
-                    </ul>
-                  </>
+                  <ul
+                    key={index}
+                    className="my-[0.12rem] flex flex-row flex-wrap gap-1 whitespace-nowrap uppercase leading-none"
+                  >
+                    <strong>{input.name}:</strong>
+                    <br className="sm:hidden" />{" "}
+                    {input.inputs.map((input) => {
+                      return (
+                        <>
+                          <code className="px-1 py-1">{input}</code>
+                        </>
+                      );
+                    })}
+                  </ul>
                 );
               })}
             </div>
